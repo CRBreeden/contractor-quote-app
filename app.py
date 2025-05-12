9fcd09f9ef136c93b3ad3b0e231fba89159e8378
CRBreeden <breedenox@gmail.com>
2025-05-12 03:23:20 -0400

    initial commit: finished logic for screening and testing about to start web dev#2

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from app.screen import screen_tickers_from_csv
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Trading App is Running!"
+    try:
+        results = screen_tickers_from_csv("tickers.csv")
+        results = sorted(results, key=lambda x: x["score"], reverse=True)
+        buys = [r for r in results if r["signal"] == "buy"]
+        sells = [r for r in results if r["signal"] == "sell"]
+        return render_template("results.html", buys=buys, sells=sells)
+    except Exception as e:
+        return f" Error: {e}"
 
 if __name__ == '__main__':
     app.run(debug=True)
